1b93a7a4f0900d233754cecf07b0d293499f9007
manuel4320 <manuel.viverosreal@campusucc.edu.co>
2025-05-30 23:19:32 -0500

    Update MedApiApplication.java

--- src/main/java/com/medicalsystem/med_api/MedApiApplication.java
+++ src/main/java/com/medicalsystem/med_api/MedApiApplication.java
@@ -3,11 +3,9 @@ package com.medicalsystem.med_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.medicalsystem")
 public class MedApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MedApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MedApiApplication.class, args);
+    }
 }
